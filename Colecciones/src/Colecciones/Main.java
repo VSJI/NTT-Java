cbe540d9f8e7f8dc2d51b65827fc7f6847a205a1
jvel <juanvs@j.com>
2022-03-15 22:09:51 +0100

    Colecciones: Hastable

--- Colecciones/src/Colecciones/Main.java
+++ Colecciones/src/Colecciones/Main.java
@@ -1,6 +1,7 @@
 package Colecciones;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -33,63 +34,78 @@ public class Main {
 			System.out.println("\nValor longitud del ArrayList: " + datos.size());
 		
 		// Stack - Pila. Es una estructura que permite guardar elementos dinamicamente dentro de ella de forma LIFO (Ultimo en entrar, primero en salir). 
-		// Se van añadiendo elementos
-			
-			// Push - Para insertar elementos en la parte posterior de la pila
-			// Pop - Eliminar elementos (último elemento añadido)
-			
-			Stack miPilaLibros = new Stack();
-			
-			miPilaLibros.push("Libro1");
-			miPilaLibros.push("Libro2");
-			miPilaLibros.push("Libro3");
-			miPilaLibros.push("Libro4");
-			
-			// En esta Pila, de forma similar a libros encima de otros en una columna, 
-			// de la cual cogeríamos los libros del que está más arriba al que está más abajo. Los libros estarían en este orden:
-			// 1. Libro4
-			// 2. Libro3
-			// 3. Libro2
-			// 4. Libro1
-			
-			// miPilaLibros.clear(); Esto limpia la pila
-			
-			String elemento;
-			
-			// miPilaLibros.pop(); Esto elimina el último elemento que fue añadido de la pila
-			
-			elemento = (String)miPilaLibros.pop();
-			
-			System.out.println("\nEl ultimo elemento añadido fue: \"" + elemento + "\", y por eso es el primer elemento que se muestra por consola");
-			
-			// Importante castear a String la pila, ya que inicialmente la definimos sin ningún tipo.
+				
+				// Se van añadiendo elementos
+				// Push - Para insertar elementos en la parte posterior de la pila
+				// Pop - Eliminar elementos (último elemento añadido)
+				
+				Stack miPilaLibros = new Stack();
+				
+				miPilaLibros.push("Libro1");
+				miPilaLibros.push("Libro2");
+				miPilaLibros.push("Libro3");
+				miPilaLibros.push("Libro4");
+				
+				// En esta Pila, de forma similar a libros encima de otros en una columna, 
+				// de la cual cogeríamos los libros del que está más arriba al que está más abajo. Los libros estarían en este orden:
+				// 1. Libro4
+				// 2. Libro3
+				// 3. Libro2
+				// 4. Libro1
+				
+				// miPilaLibros.clear(); Esto limpia la pila
+				
+				String elemento;
+				
+				// miPilaLibros.pop(); Esto elimina el último elemento que fue añadido de la pila
+				
+				elemento = (String)miPilaLibros.pop();
+				
+				System.out.println("\nEl ultimo elemento añadido fue: \"" + elemento + "\", y por eso es el primer elemento que se muestra por consola");
+				
+				// Importante castear a String la pila, ya que inicialmente la definimos sin ningún tipo.
 			
 		// Queue - Cola. FIFO, First In First Out. IMPORTANTE, no es recomendable fijar un tamaño ya que es dinámico, aunque se puede
+			
 				// -> Add - Para insertar elementos en la cola
 				// -> poll - Para eliminar el primer elemento de la cola
 				// -> peek - Para consultar el primer elemento de la cola
 			
 				// Ejemplo de contexto: la fila de un cine para comprar entradas.
 			
-			//Queue NO es una clase, es una interfaz
+				//Queue NO es una clase, es una interfaz, por eso no es correcto "new Queue()" sino que se usa "new LinkedList()"
+				
+				Queue cola = new LinkedList();
+				
+				// Restringir la cola con tipo Integer:
+				Queue<Integer> cola2 = new LinkedList();
+				
+				cola.add(5);
+				cola.add(10);
+				cola.add(15);
+				
+				System.out.println("Cola:\n" + cola + "\n");
+				System.out.println("Primer elemento de la cola:\n" + cola.peek() + "\n");
+				System.out.println("Eliminamos elemento de la cola: \"" + cola.poll() + "\", y queda en la cola:\n" + cola + "\n");
 			
-			Queue cola = new LinkedList();
-			
-			// Restringir la cola con tipo Integer:
-			Queue<Integer> cola2 = new LinkedList();
-			
-			cola.add(5);
-			cola.add(10);
-			cola.add(15);
-			
-			System.out.println("Cola:\n" + cola + "\n");
-			System.out.println("Primer elemento de la cola:\n" + cola.peek() + "\n");
-			System.out.println("Eliminamos elemento de la cola: \"" + cola.poll() + "\", y queda en la cola:\n" + cola);
 			
 			
-			
-		// HastTable
+		// HastTable <K, V> K=Key, V=Value
 		
+				Hashtable tabla2 = new Hashtable<>();
+				
+				// Este Hastable que creamos es para contenter Enteros para nuestras KEYS y Strings para nuestros VALORES
+				// Hashtable<Integer, String> tabla2 = new Hashtable<>(); 
+				
+				tabla2.put(1, "Apple");
+				tabla2.put(2, "Sony");
+				tabla2.put(6, "Samsung");
+				
+				tabla2.put("Ferrari", 400);
+				
+				System.out.println("Hashtable:\n" + tabla2 + "\n");
+				System.out.println("Hashtable tabla2.get(2):\n" + tabla2.get(2) + "\n");
+				System.out.println("Hashtable tabla2.get(Ferrari):\n" + tabla2.get("Ferrari") + "\n");
 	}
 
 }
